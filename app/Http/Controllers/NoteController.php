be13726ee1336ec7407531d32eff04d75874dd67
hzwnyhrn <hzwnyhrn99@gmail.com>
2017-10-30 16:32:52 +0800

    First Commit

---
+++ app/Http/Controllers/NoteController.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Note;
+use Illuminate\Http\Request;
+
+class NoteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $notes = Note::all();
+        return view('notes.create', compact ('notes'));
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'title' => 'required',
+            'body' => 'required|max:100'
+
+        ]);
+
+        // save to database
+        $note = new Note;
+
+        $note->title = $request->title;
+        $note->body = $request->body;
+
+        $note->save();
+
+
+        //Return view
+
+        return redirect()->route('notes.create');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/notes/create.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+
+    
+
+                <div class="panel-heading">Create Notes</div>
+
+                <div class="panel-body">
+                    
+
+                    <form method="POST" action="{{route('notes.store') }}" enctype="multipart/form-data">
+                    {{ csrf_field() }}
+
+                        <div class="form-group">
+                            <label for="title">Title :<label>
+                            <input type="text" class="form-control" name="title">
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="body">Body :<label>
+                            <input type="text" class="form-control" name="body">
+                        </label>
+
+                        
+                        <button type="submit" class="btn btn-success">Post</button>
+                    </form>
+                </div>
+
+                
+
+            </div>
+
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
